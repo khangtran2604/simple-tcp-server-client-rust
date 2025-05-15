fafec6b720419e749a182dab19754e42d0228447
Tran Manh Khang <khang.tm@MBAM1-2021007.local>
2025-05-15 13:26:30 +0700

    implement + unit test
    - post_tutor_db
    - get_tutors_db
    - get_tutor_by_id_db

--- src/db_access/tutor.rs
+++ src/db_access/tutor.rs
@@ -1,5 +1,149 @@
+use std::fmt::format;
+
 use sqlx::SqliteConnection;
 
-use crate::models::tutor::NewTutorInput;
+use crate::custom_error::CustomError;
+use crate::models::tutor::{NewTutorInput, Tutor};
 
-pub fn post_tutor_db(&mut conn: SqliteConnection, data: NewTutorInput) {}
+pub async fn post_tutor_db(
+    conn: &mut SqliteConnection,
+    data: NewTutorInput,
+) -> Result<Tutor, CustomError> {
+    let query_str = format!(
+        "
+    INSERT INTO tutor(name, age) VALUES('{}', {})
+    RETURNING id, name, age, created_at
+",
+        data.name, data.age
+    );
+
+    let created_tutor = sqlx::query_as::<_, Tutor>(&query_str)
+        .fetch_one(conn)
+        .await
+        .map_err(|e| CustomError::SqlxError(e.to_string()))?;
+
+    Ok(created_tutor)
+}
+
+pub async fn get_tutors_db(conn: &mut SqliteConnection) -> Result<Vec<Tutor>, CustomError> {
+    let query_str = "SELECT * from tutor";
+
+    let tutors = sqlx::query_as::<_, Tutor>(query_str)
+        .fetch_all(conn)
+        .await
+        .map_err(|e| CustomError::SqlxError(e.to_string()))?;
+
+    Ok(tutors)
+}
+
+pub async fn get_tutor_by_id_db(
+    conn: &mut SqliteConnection,
+    tutor_id: &str,
+) -> Result<Option<Tutor>, CustomError> {
+    let query_str = format!(
+        "
+SELECT * from tutor WHERE id = {}
+",
+        tutor_id
+    );
+
+    let result = sqlx::query_as::<_, Tutor>(&query_str)
+        .fetch_optional(conn)
+        .await
+        .map_err(|e| CustomError::SqlxError(e.to_string()))?;
+
+    Ok(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use dotenv::dotenv;
+    use fake::{Fake, faker::name::en::FirstName};
+
+    use crate::db_access::get_db_conn;
+
+    use super::*;
+
+    #[actix_web::test]
+    async fn test_post_tutor_db_successfully() {
+        dotenv().ok();
+
+        let mut conn = get_db_conn().await;
+        let new_tutor_name: String = FirstName().fake();
+        let new_tutor_age: i32 = (20..40).fake();
+
+        let new_tutor_input = NewTutorInput {
+            name: new_tutor_name,
+            age: new_tutor_age,
+        };
+
+        let result = post_tutor_db(&mut conn, new_tutor_input.clone()).await;
+
+        println!("{:?}", result);
+
+        assert!(result.is_ok());
+
+        let created_tutor = result.unwrap();
+
+        assert_eq!(created_tutor.name, new_tutor_input.name);
+        assert_eq!(created_tutor.age, new_tutor_input.age);
+    }
+
+    #[actix_web::test]
+    async fn test_get_tutors_db() {
+        dotenv().ok();
+
+        let mut conn = get_db_conn().await;
+        let result = get_tutors_db(&mut conn).await;
+
+        assert!(result.is_ok());
+
+        let tutors = result.unwrap();
+
+        assert!(!tutors.is_empty())
+    }
+
+    #[actix_web::test]
+    async fn test_get_tutor_by_id_successfully() {
+        dotenv().ok();
+
+        let mut conn = get_db_conn().await;
+        let new_tutor_name: String = FirstName().fake();
+        let new_tutor_age: i32 = (20..40).fake();
+
+        let new_tutor_input = NewTutorInput {
+            name: new_tutor_name,
+            age: new_tutor_age,
+        };
+
+        let created_tutor = post_tutor_db(&mut conn, new_tutor_input).await.unwrap();
+
+        let find_result = get_tutor_by_id_db(&mut conn, created_tutor.id.to_string().trim()).await;
+
+        assert!(find_result.is_ok());
+
+        let option_tutor = find_result.unwrap();
+        assert!(option_tutor.is_some());
+
+        let tutor = option_tutor.unwrap();
+
+        assert_eq!(created_tutor, tutor);
+    }
+
+    #[actix_web::test]
+    async fn test_get_non_exist_tutor_by_id() {
+        dotenv().ok();
+
+        let mut conn = get_db_conn().await;
+        let non_exist_tutor_id = "99999999991";
+
+        let result = get_tutor_by_id_db(&mut conn, non_exist_tutor_id).await;
+        println!("{:?}", result);
+
+        assert!(result.is_ok());
+
+        let option_tutor = result.unwrap();
+
+        assert!(option_tutor.is_none());
+    }
+}
--- src/models/tutor.rs
+++ src/models/tutor.rs
@@ -1,18 +1,24 @@
 use serde::{Deserialize, Serialize};
 use sqlx::types::chrono::{DateTime, Utc};
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, sqlx::FromRow, PartialEq)]
 pub struct Tutor {
     pub id: i32,
     pub name: String,
     pub age: i32,
-    pub create_at: DateTime<Utc>,
+    pub created_at: DateTime<Utc>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct NewTutorInput {
     pub name: String,
     pub age: i32,
+}
+
+impl NewTutorInput {
+    pub fn new(name: String, age: i32) -> Self {
+        Self { name, age }
+    }
 }
 
 #[derive(Debug, Deserialize)]
